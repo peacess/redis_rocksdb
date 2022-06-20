1956061346945c5eab0a47fb758dadc58709c94d
gcxfd <i@rmw.link>
2022-06-20 15:28:45 +0800

    🔶

--- src/hash.rs
+++ src/hash.rs
@@ -1,1 +1,7 @@
+use crate::{Bytes, LenType, RrError};
 
+pub trait RedisHash {
+    fn hget<K: Bytes, V: Bytes>(&self, key: &K) -> Result<Option<Vec<u8>>, RrError>;
+    fn hset<K: Bytes, V: Bytes>(&mut self, key: &K, value: &V) -> Result<(), RrError>;
+    fn hlen<K: Bytes>(&mut self, key: &K) -> Result<LenType, RrError>;
+}
