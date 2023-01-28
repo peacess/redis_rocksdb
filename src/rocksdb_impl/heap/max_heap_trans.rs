f0bfc3a004a80bbc6b0f45369a588bbfb7ea7d7a
Peace <peacess@163.com>
2023-01-28 23:51:21 +0800

    binary heap

---
+++ src/rocksdb_impl/heap/max_heap_trans.rs
@@ -0,0 +1,62 @@
+use rocksdb::{Transaction, TransactionDB};
+
+use crate::{Heap, RrError};
+use crate::rocksdb_impl::heap::heap::{FieldHeap, MaxHeapCompare};
+use crate::rocksdb_impl::shared::{make_head_key, make_key};
+
+/// 字段名使用 max head存放
+pub struct MaxHeapTrans {}
+
+impl<'db> Heap<Transaction<'db, TransactionDB>> for MaxHeapTrans {
+    fn pop(&self, t: &Transaction<'db, TransactionDB>, key: &[u8]) -> Result<Option<(Vec<u8>, Vec<u8>)>, RrError> {
+        let head_key = make_head_key(key);
+        let mut heap = {
+            match t.get(&head_key)? {
+                None => return Ok(None),
+                Some(v) => FieldHeap::new(v)
+            }
+        };
+        let p = &mut heap as *mut _;
+        heap.init(MaxHeapCompare { heap: p });
+        let field = match heap.pop() {
+            None => return Ok(None),
+            Some(f) => f
+        };
+        let field_key = make_key(key, &field);
+        let v = {
+            match t.get(field_key)? {
+                None => vec![],
+                Some(v) => v
+            }
+        };
+        t.put(&head_key, &heap.data)?;
+        Ok(Some((field, v)))
+    }
+
+    fn push(&self, t: &Transaction<'db, TransactionDB>, key: &[u8], field: &[u8], value: &[u8]) -> Result<(), RrError> {
+        let field_key = make_key(key, field);
+        /// 由于[rocksdb::TransactionDB]没有[rocksdb::DB::key_may_exist]方法，所以只能取一次key value
+        if t.get(&field_key)?.is_none() {
+            let head_key = make_head_key(key);
+            let mut heap = {
+                match t.get(&head_key)? {
+                    None => FieldHeap::new(vec![]),
+                    Some(v) => FieldHeap::new(v)
+                }
+            };
+            let p = &mut heap as *mut _;
+            heap.init(MaxHeapCompare { heap: p });
+            heap.push(field);
+            t.put(&head_key, &heap.data)?;
+        }
+        t.put(&field_key, value)?;
+        Ok(())
+    }
+}
+
+
+
+
+
+
+
--- src/rocksdb_impl/heap/mod.rs
+++ src/rocksdb_impl/heap/mod.rs
@@ -1,6 +1,10 @@
 pub use max_heap::*;
+pub use max_heap_db::*;
+pub use max_heap_trans::*;
 pub use min_heap::*;
 
 mod max_heap;
 mod heap;
 mod min_heap;
+mod max_heap_trans;
+mod max_heap_db;
